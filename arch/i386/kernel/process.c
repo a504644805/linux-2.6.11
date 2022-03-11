4d5912dbedd00cc93c6216512fe097ab67b7e63f
a504644805 <45729823+a504644805@users.noreply.github.com>
2022-03-11 08:06:04 +0800

    Update process.c

--- arch/i386/kernel/process.c
+++ arch/i386/kernel/process.c
@@ -309,7 +309,7 @@ int kernel_thread(int (*fn)(void *), void * arg, unsigned long flags)
 	regs.eip = (unsigned long) kernel_thread_helper;
 	regs.xcs = __KERNEL_CS;
 	regs.eflags = X86_EFLAGS_IF | X86_EFLAGS_SF | X86_EFLAGS_PF | 0x2;
-
+	//不需要管ss3和esp3，因为cs是在0特权级的，因此iret不会去置ss3和esp3
 	/* Ok, create the new process.. */
 	return do_fork(flags | CLONE_VM | CLONE_UNTRACED, 0, &regs, 0, NULL, NULL);
 }
